b654006099c4cbec3ae8feb8bedd72d5556ea39d
Smartkeyss <smartrhoda95@gmail.com>
2022-12-09 07:02:26 +0100

    assignment

---
+++ python_part1/Condition3.py
@@ -0,0 +1,8 @@
+X = int(input("Enter a number:"))
+Y = int(input("Enter a number:"))
+if X > Y:
+    print("X greater than Y")
+elif X < Y:
+    print("X less than Y")
+else:
+    print("X equals to Y")
---
+++ python_part1/condition4.py
@@ -0,0 +1,6 @@
+X = int(input("Enter a number:"))
+Y = int(input("Enter a number:"))
+if abs(X) > abs(Y):
+    print("X's absolute value greater than Y's absolute value")
+else:
+    print("Y's absolute value greater than X's absolute value ")
