495c2fbac4a6046b23c30e0b93f13a66b5e478b1
Smartkeyss <smartrhoda95@gmail.com>
2022-12-09 09:05:13 +0100

    assignent

---
+++ python_part1/method3.py
@@ -0,0 +1,28 @@
+num1 = 1122334455666
+#1
+num1_str=str(num1) 
+#2
+print(len(num1_str))
+#3
+print(num1_str[2])                               #3rd question
+#4
+print(num1_str[2:5])
+#5
+if "2" in string_with_0:
+    print("Number 2 existing in string_with_0")
+else:
+    print("Number 2 is not exist")
+#6
+if "3" in string_with_0:
+    print("Number 3 existing in string_with_0")
+else:
+    print("Number 3 is not exist")
+#7
+string_with_0="0"+num1_str
+print(string_with_0)
+#8
+print(string_with_0[0:3])
+#9
+print(string_with_0[5:len(string_with_0)])
+#10
+print(string_with_0[-7:-4])
