a0bc6089e12b45980d4a7e18214cc68f29d94cc1
Smartkeyss <smartrhoda95@gmail.com>
2022-12-28 09:29:45 +0100

    assignment correctio

--- python_part2/iterators_for2.py
+++ python_part2/iterators_for2.py
@@ -1,3 +1,5 @@
-X = "COFFEE BREAK"
-for i in X:
-    print(i)
+todo = ["exercise1", "exercise2", "exercise3","coffee break" ,"exercise4","exercise5","exercise6"]
+for x in todo:
+    if x.capitalize == "COFFEE BREAK":
+        print(x)
+        break
